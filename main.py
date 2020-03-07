0344229e3b7540adcae5e30ae53adff10980ab90
kulvirvirk <kvirkdev@gmail.com>
2020-03-07 06:41:20 +0000

    added the leap year logic

--- main.py
+++ main.py
@@ -1,3 +1,16 @@
 # Problem description: 
 # https://www.hackerrank.com/challenges/write-a-function/problem 
 
+def is_leap(year):
+  leap = False
+  if year % 4 == 0:
+    leap = True
+    if year % 100 == 0 and year % 400 != 0:
+      leap = False
+
+  else: leap == False
+  return leap
+
+year = int(input('Enter year: '))
+
+print(is_leap(year))
