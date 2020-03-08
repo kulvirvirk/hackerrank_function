e63d162d8cc6a0330245ff8dc307cac0b280a0af
kulvirvirk <kvirkdev@gmail.com>
2020-03-08 03:05:23 +0000

    added second method; better way to return the condition

--- main.py
+++ main.py
@@ -2,14 +2,21 @@
 # https://www.hackerrank.com/challenges/write-a-function/problem 
 
 def is_leap(year):
-  leap = False
-  if year % 4 == 0:
-    leap = True
-    if year % 100 == 0 and year % 400 != 0:
-      leap = False
 
-  else: leap == False
-  return leap
+  # First method; works but unnecessarily long 
+  # leap = False
+  # if year % 4 == 0:
+  #   leap = True
+  #   if year % 100 == 0 and year % 400 != 0:
+  #     leap = False
+
+  # else: leap == False
+  # return leap
+
+ # second method; way more elegent.
+ # because we are just returning True or False 
+  return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
+
 
 year = int(input('Enter year: '))
 
